aa306f1ef65d014eb9cd0b623022f86fcfde27fd
etwillbefine <tim.gatzemeier@gmx.de>
2019-08-18 15:41:59 +0200

    correct output references

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,11 @@ output "vpc_id" {
 }
 
 output "vpc_cidr_block" {
-    value = module.vpc.vpc_cidr_block
+    value = module.vpc.cidr_block
 }
 
 output "vpc_cidr_ipv6_block" {
-    value = module.vpc.ipv6_vpc_cidr_block
+    value = module.vpc.ipv6_cidr_block
 }
 
 output "public_subnet_ids" {
